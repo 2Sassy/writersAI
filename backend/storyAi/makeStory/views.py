4110f9bf6e7d7b52aa315059c70bb8716fe26779
Sourcery AI <>
2022-08-25 23:24:18 +0000

    'Refactored by Sourcery'

--- backend/storyAi/makeStory/views.py
+++ backend/storyAi/makeStory/views.py
@@ -20,44 +20,46 @@ from keras.utils import np_utils
 
 class WriterMind(object):
     
-    def read_vocab(path):
-        file = open(path) 
-        chars = file.read().split(" ")
-        chars.insert(1, " ")
-        file.close()
-
+    def read_vocab(self):
+        with open(self) as file:
+            chars = file.read().split(" ")
+            chars.insert(1, " ")
         return chars
 
-    def vocab_processing(vocab):
+    def vocab_processing(self):
         char2idx = {u:i for i, u in enumerate(vocab)}
-        idx2char = np.array(vocab)
+        idx2char = np.array(self)
 
         return char2idx, idx2char
 
-    def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
-        model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(vocab_size, embedding_dim,
-                                batch_input_shape=[batch_size, None]),
-        tf.keras.layers.GRU(rnn_units,
-                            return_sequences=True,
-                            stateful=True,
-                            recurrent_initializer='glorot_uniform'),
-        tf.keras.layers.Dense(vocab_size) 
-        ])
-        return model
+    def build_model(self, embedding_dim, rnn_units, batch_size):
+        return tf.keras.Sequential(
+            [
+                tf.keras.layers.Embedding(
+                    self, embedding_dim, batch_input_shape=[batch_size, None]
+                ),
+                tf.keras.layers.GRU(
+                    rnn_units,
+                    return_sequences=True,
+                    stateful=True,
+                    recurrent_initializer='glorot_uniform',
+                ),
+                tf.keras.layers.Dense(self),
+            ]
+        )
 
-    def generate_text(model, start_string, num_generate, char2idx, idx2char):
-		    
+    def generate_text(self, start_string, num_generate, char2idx, idx2char):
+    		
         input_eval = [char2idx[s] for s in start_string]
         input_eval = tf.expand_dims(input_eval, 0)
 
         text_generated = []
         temperature = 1.0
 
-        model.reset_states()
-        for i in range(num_generate):
-            predictions = model(input_eval)
-            
+        self.reset_states()
+        for _ in range(num_generate):
+            predictions = self(input_eval)
+
             predictions = tf.squeeze(predictions, 0)
             predictions = predictions / temperature
             predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
@@ -70,27 +72,27 @@ class WriterMind(object):
 
 class WriterAnswer(object):
 
-    def create_story(data):
-        
+    def create_story(self):
+
         embedding_dim = 256
         rnn_units = 1048
         katalog = "AI_weights"
-        author = data['author']
-        text = data['storyBegin']
-        vocab_path = os.getcwd()+ "/" +katalog+ "/" +author +"/vocab.txt"
-        weights_path = os.getcwd()+ "/" +katalog+ "/" +author +"/ckpt_100"
+        author = self['author']
+        text = self['storyBegin']
+        vocab_path = f"{os.getcwd()}/{katalog}/{author}/vocab.txt"
+        weights_path = f"{os.getcwd()}/{katalog}/{author}/ckpt_100"
 
         tf.compat.v1.enable_eager_execution()
 
         chars = WriterMind.read_vocab(vocab_path)
         vocab_size = len(chars)
 
-        model = WriterMind.build_model(vocab_size, embedding_dim, rnn_units, batch_size=1) 
+        model = WriterMind.build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
         char2idx, idx2char = WriterMind.vocab_processing(chars)
-        
+
         model.load_weights(weights_path)
 
-        data = WriterMind.generate_text(model, text[-100:], 600, char2idx, idx2char)
+        self = WriterMind.generate_text(model, text[-100:], 600, char2idx, idx2char)
 
-        return data
+        return self
     
